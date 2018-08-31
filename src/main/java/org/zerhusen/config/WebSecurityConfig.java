772311fee2c56c2a972ffdff7263173ec99463e5
claurier <carl.laurier@sword-group.com>
2018-08-31 18:00:43 +0200

    // jwt.route.authentication.path = auth

--- src/main/java/org/zerhusen/config/WebSecurityConfig.java
+++ src/main/java/org/zerhusen/config/WebSecurityConfig.java
@@ -38,6 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${jwt.header}")
     private String tokenHeader;
 
+    // jwt.route.authentication.path = auth
     @Value("${jwt.route.authentication.path}")
     private String authenticationPath;
 
